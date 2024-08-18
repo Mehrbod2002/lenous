da131e6a866a61b6f66282ecd9502cccbf97f67a
Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>
2024-08-18 22:35:33 +0330

    chore: initial
    Signed-off-by: Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>

--- tests/lenous.ts
+++ tests/lenous.ts
@@ -1,16 +1,23 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Lenous } from "../target/types/lenous";
+import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 
-describe("lenous", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const idlPath = resolve('./../target/idl/lenous.json');
+const idl = JSON.parse(readFileSync(idlPath, 'utf8'));
+const wallet = new anchor.Wallet();
+const programId = new PublicKey("F9GEX6D75oQA5h1Scn4WgH7zFxCkeVCn2HMSnrbhfH6Z");
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+const provider = new anchor.AnchorProvider(connection, wallet, {});
 
-  const program = anchor.workspace.Lenous as Program<Lenous>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+const program = new Program<Lenous>(IDL, programId, {
+  connection,
 });
+
+function resolve(arg0: string) {
+  throw new Error("Function not implemented.");
+}
+function readFileSync(idlPath: void, arg1: string): string {
+  throw new Error("Function not implemented.");
+}
+
